66b5c59437581c09056730d16c4c4b41278046c8
Youness Rziqi <rziqiyouness@gmail.com>
2026-02-26 07:58:32 +0000

    Updata

---
+++ 125) Practice/level2/ft_range.c
@@ -0,0 +1,75 @@
+/*
+Assignment name  : ft_range
+Expected files   : ft_range.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int     *ft_range(int start, int end);
+
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at start and end at end (Including start and end !), then
+return a pointer to the first value of the array.
+
+Examples:
+
+- With (1, 3) you will return an array containing 1, 2 and 3.
+- With (-1, 2) you will return an array containing -1, 0, 1 and 2.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing 0, -1, -2 and -3.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int *ft_range(int start, int end)
+{
+    int *p;
+    int len = 0;
+    int i = 0;
+
+    if (start >= end)
+        len = start - end + 1;
+    else
+        len = end - start + 1;
+
+    p = malloc(sizeof(int) * len);
+    if (!p)
+        return NULL;
+
+    while (i < len)
+    {
+        p[i] = start;
+        if (start >= end)
+            start--;
+        else
+            start++;
+
+        i++;
+    }
+    return p;
+}
+
+int main()
+{
+    int start = 0;
+    int end = -3;
+    int len;
+    if (start < end)
+        len = end - start + 1;
+    else
+        len = start - end + 1;
+    int i = 0;
+
+    int *arr = ft_range(start, end);
+
+    while (i < len)
+    {
+        printf("%d ", arr[i]);
+        i++;
+    }
+
+    free(arr);
+    return 0;
+}
